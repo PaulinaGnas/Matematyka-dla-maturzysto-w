6e4e9d45b112e815d335c11541383d487480a1d2
Paulina Gnas <bubelx2@gmail.com>
2021-12-15 19:33:11 +0100

    CourseCell and custom cell

--- Matematyka dla maturzystów/Controllers/MenuViewController.swift
+++ Matematyka dla maturzystów/Controllers/MenuViewController.swift
@@ -8,7 +8,36 @@
 import UIKit
 
 class MenuViewController: UIViewController {
+    
+    @IBOutlet weak var infoLabel: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.dataSource = self
+        tableView.register(UINib(nibName: C.cellNibName, bundle: nil), forCellReuseIdentifier: C.cellIdentifier)
+        tableView.delegate = self
+    }
+    
+    let courseBrain = CourseBrain()
+}
+
+extension MenuViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        courseBrain.getNumber()
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: C.cellIdentifier, for: indexPath) as! CourseCell
+        
+        cell.courseLabel.text = courseBrain.getName(index: indexPath.row)
+        
+        return cell
+    }
+}
+
+extension MenuViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(indexPath.row)
     }
 }
---
+++ Matematyka dla maturzystów/Models/Course.swift
@@ -0,0 +1,14 @@
+//
+//  Course.swift
+//  Matematyka dla maturzystów
+//
+//  Created by Paulina Gnas on 15/12/2021.
+//
+
+import Foundation
+
+struct Course {
+    
+    let name: String
+    let info: String
+}
